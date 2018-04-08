c2f9dabe7776edad7a3cc4bdf46be08c5ab51770
TedtheDev <tylerjedmunds@gmail.com>
2018-04-08 15:21:16 -0500

    took out console log

--- src/containers/added_currency_container.js
+++ src/containers/added_currency_container.js
@@ -15,7 +15,6 @@ class AddedCurrencyContainer extends Component {
 
     onChangeAmountOwned(event) {
         if(!isNaN(event.target.value)) {
-            console.log(event.target.value)
             const previousValue = (event.target.defaultValue || Number(event.target.defaultValue)) * Number(this.props.coin.price_usd);
             const updatedValue = Number(event.target.value) * Number(this.props.coin.price_usd);
             this.props.updateTotalAmountOwnedUSD(previousValue, updatedValue);
