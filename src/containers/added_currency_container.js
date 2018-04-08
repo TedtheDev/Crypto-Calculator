e944f16718078c9fc8a90d6e407d562e21e94867
TedtheDev <tylerjedmunds@gmail.com>
2018-04-08 15:14:00 -0500

    fixed amount owned input with decimal, etc

--- src/containers/added_currency_container.js
+++ src/containers/added_currency_container.js
@@ -14,12 +14,13 @@ class AddedCurrencyContainer extends Component {
     }
 
     onChangeAmountOwned(event) {
-        //if(!isNaN(event.target.value)) {
+        if(!isNaN(event.target.value)) {
+            console.log(event.target.value)
             const previousValue = (event.target.defaultValue || Number(event.target.defaultValue)) * Number(this.props.coin.price_usd);
             const updatedValue = Number(event.target.value) * Number(this.props.coin.price_usd);
             this.props.updateTotalAmountOwnedUSD(previousValue, updatedValue);
-            this.setState({amountOwned: Number(event.target.value)});
-        //}
+            this.setState({amountOwned: event.target.value});
+        }
     }
 
     render() {
