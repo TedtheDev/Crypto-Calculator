ec9f9971d795f4cd8f5b4f674406d90da2d89ea0
TedtheDev <tylerjedmunds@gmail.com>
2018-04-08 15:21:49 -0500

    converted price per coin to a USD string value

--- src/components/added_currency.js
+++ src/components/added_currency.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Input } from 'semantic-ui-react';
 
 const AddedCurrency = ({coin, amountOwned, onChangeAmountOwned, removeCoin}) => {
+
+    // coin price in USD multiply amount owned to get totalAmountOwned in USD per coin
     const totalAmountOwnedUSD = Number(coin.price_usd) * Number(amountOwned);
+    // convert price_usd to a nice dollar value that is rounded
+    const coinPriceUSD = Number(coin.price_usd).toLocaleString('en', { style: 'currency', currency: 'USD'})
+
     return [
         <div key={coin.name}>{coin.name}</div>,
         <Input key={`${coin.name}-amount`} placeholder="Enter Amount Owned" onChange={onChangeAmountOwned} value={amountOwned.toString()}></Input>,
-        <div key={`${coin.name}-price`}>{coin.price_usd.toLocaleString('en', { style: 'currency', currency: 'USD'})}</div>,
+        <div key={`${coin.name}-price`}>{coinPriceUSD}</div>,
         <div key={`${coin.name}-total`}>{totalAmountOwnedUSD.toLocaleString('en', { style: 'currency', currency: 'USD'})}</div>,
         <div key={`${coin.name}-remove`}><button onClick={() => removeCoin(coin.id, Number(coin.price_usd) * Number(amountOwned))}>Remove</button></div>
     ]
