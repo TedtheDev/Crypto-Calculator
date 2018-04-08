761f963047a9206a21eaabbb39345c202a96072e
TedtheDev <tylerjedmunds@gmail.com>
2018-04-08 17:38:53 -0500

    change net profit text color to red or green based on negative or positive profit

--- src/components/total_net_profit.js
+++ src/components/total_net_profit.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const TotalNetProfit = ({netProfit}) => {
+    
+    const NetProfitDiv = styled.div`
+        color: ${ netProfit < 0 ? `red` : `green` };
+    `;
+
     return (
-        <div>Net Profit: {Number(netProfit).toLocaleString('en', { style: 'currency', currency: 'USD'})}</div>
+        <div>Net Profit: <NetProfitDiv>{Number(netProfit).toLocaleString('en', { style: 'currency', currency: 'USD'})}</NetProfitDiv></div>
     )
 };
 
